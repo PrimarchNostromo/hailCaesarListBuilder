0a59536ba5d6b14e9f2828901462258b3661a644
Joshua Geyer <jsg277@nau.edu>
2021-09-09 13:15:08 -0700

    Update republicanRomansScript.js

--- republicanRomansScript.js
+++ republicanRomansScript.js
@@ -1,15 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 function fun()
 {
  var ddl = document.getElementById("dropdown");
  var selectedValue = ddl.options[ddl.selectedIndex].value;
 
- document.getElementById("unitBox").innerHTML = "fuck";
- document.write(selectedValue);
+ document.getElementById("unitBox").innerHTML = selectedValue;
+}
+
+
+function changeColor(color) {
+   document.querySelector(".name").style.color = color;
 }
