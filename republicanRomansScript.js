25de6cca1c28d499fe66354dab54a58dd7bf71cd
Joshua Geyer <jsg277@nau.edu>
2021-09-20 13:08:14 -0700

    Update republicanRomansScript.js

--- republicanRomansScript.js
+++ republicanRomansScript.js
@@ -1,22 +1,29 @@
-function fun()
-{
- var ddl = document.getElementById("dropdown");
- var selectedValue = ddl.options[ddl.selectedIndex].value;
-
- document.getElementById("unitBox").innerHTML = selectedValue;
-}
+//global variable for the list part
+var value = 1;
 
 
-function changeColor(color) {
-   document.querySelector(".name").style.color = color;
-}
 
+
+//add a unit to the list
 function addItem(name, points) {
    // document.getElementById("unitBox").innerHTML += "<br>";
     var ddl = document.getElementById("dropdown");
     var selectedValue = ddl.options[ddl.selectedIndex].value;
  
-    document.getElementById(selectedValue).innerHTML += "<br>" + name + " " + points;
+    //add the item to the list itself
+    document.getElementById(selectedValue).innerHTML += "<div id='" + window.value + "'>" + name + " " + points 
+            + "<button type='button' onclick='removeItem(" + window.value + "," + points + ")'>Delete</button></div>";
+    window.value++;
+    //update points
     document.getElementById("pointsTotal").innerHTML = 
             parseFloat(document.getElementById("pointsTotal").innerHTML) + parseFloat(points);
 }
+
+//delete a unit from the list
+function removeItem(id, points){
+    var div = document.getElementById(id);
+    div.remove();
+    //update points
+    document.getElementById("pointsTotal").innerHTML = 
+            parseFloat(document.getElementById("pointsTotal").innerHTML) - parseFloat(points);
+    }
